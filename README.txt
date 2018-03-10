515457eabf9c15131a16b46351c0564cfa7e06f5
yassers-game <36106744+yassers-game@users.noreply.github.com>
2018-03-10 15:43:14 -0500

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/tompark/pen/OzzJJj.
+
+ Multiplayer client example rendering multiple players using socket.io
---
+++ js/index.js
@@ -0,0 +1,60 @@
+// Connect to the server
+var address = "https://server-djwxqxfveu.now.sh";
+var socket = io(address);
+
+socket.on("connect", function () {
+  return socket.emit('newPlayer');
+});
+socket.on("state", render);
+
+// Render the Game
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+function render(players) {
+  // Clear prior frame
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Draw players
+  ctx.fillStyle = "red";
+  for (id in players) {
+    var player = players[id];
+    ctx.fillRect(player.x, player.y, 50, 50);
+  }
+}
+
+// Movement
+var movement = {
+  up: false,
+  left: false,
+  right: false,
+  down: false
+};
+
+function broadcastMovement() {
+  socket.emit("movement", movement);
+}
+
+// Controls
+
+var keymap = {
+  87: "up",
+  65: "left",
+  83: "down",
+  68: "right"
+};
+
+function keydown(event) {
+  var direction = keymap[event.keyCode];
+  if (direction) movement[direction] = true;
+}
+
+function keyup(event) {
+  var direction = keymap[event.keyCode];
+  if (direction) movement[direction] = false;
+}
+
+window.addEventListener("keydown", keydown);
+window.addEventListener("keyup", keyup);
+
+setInterval(broadcastMovement, 1000 / 60);
